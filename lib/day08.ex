2fd7cff4bc466ca930afef85a73052cf8d57c968
Baertschi, Adrian <adrian.baertschi@zuehlke.com>
2022-12-08 17:20:30 +0100

    Day 08 - refactoring

--- lib/day08.ex
+++ lib/day08.ex
@@ -1,7 +1,6 @@
 defmodule Day08 do
   def part_one(input) do
-    array = input |> parse()
-    l = Enum.count(array) - 1
+    {array, l} = input |> parse()
 
     for x <- Range.new(0, l) do
       for y <- Range.new(0, l) do
@@ -13,50 +12,45 @@ defmodule Day08 do
   end
 
   def part_two(input) do
-    array = input |> parse()
-    l = Enum.count(array) - 1
+    {array, l} = input |> parse()
 
     for x <- Range.new(0, l) do
       for y <- Range.new(0, l) do
-        viewing_dist(x, y, l, array)
+        parts = neighbours_two(x, y, l, array)
+        e = array |> Enum.at(x) |> Enum.at(y)
+        calc_dist(parts, e)
       end
     end
     |> List.flatten()
     |> Enum.max()
   end
 
-  defp viewing_dist(x, y, l, array) do
-    parts = neighbours_two(x, y, l, array)
-    e = array |> Enum.at(x) |> Enum.at(y)
-    calc_dist(parts, e)
-  end
-
-  defp calc_dist(parts, e) do
-    parts
-    |> Enum.map(fn el -> dist(el, e) end)
-    |> Enum.product()
-  end
-
-  defp dist(list, e) do
-    Enum.reduce_while(list, 0, fn x, acc ->
-      if x >= e, do: {:halt, acc + 1}, else: {:cont, acc + 1}
-    end)
-  end
-
   defp visible?(x, y, l, array) do
-    case {x, y} do
-      {x, _y} when x in [0, l] ->
-        true
+    if x == 0 || x == l || y == 0 || y == l do
+      true
+    else
+      e = array |> Enum.at(x) |> Enum.at(y)
 
-      {_x, y} when y in [0, l] ->
-        true
-
-      _ ->
-        e = array |> Enum.at(x) |> Enum.at(y)
-
-        neighbours_one(x, y, l, array)
-        |> Enum.any?(fn list -> Enum.all?(list, fn ne -> e > ne end) == true end)
+      neighbours_one(x, y, l, array)
+      |> Enum.any?(fn list -> Enum.all?(list, fn ne -> e > ne end) == true end)
     end
+  end
+
+  defp neighbours_one(x, y, l, array) do
+    up =
+      for i <- Range.new(0, x - 1) do
+        array |> Enum.at(i) |> Enum.at(y)
+      end
+
+    down =
+      for i <- Range.new(x + 1, l) do
+        array |> Enum.at(i) |> Enum.at(y)
+      end
+
+    right = array |> Enum.at(x) |> Enum.slice(y + 1, l - 1)
+    left = array |> Enum.at(x) |> Enum.slice(0, y)
+
+    [up, down, right, left]
   end
 
   defp neighbours_two(x, y, l, array) do
@@ -79,37 +73,31 @@ defmodule Day08 do
         end
       end
 
-    right = Enum.at(array, x) |> Enum.slice(y + 1, l)
-    left = Enum.at(array, x) |> Enum.slice(0, y) |> Enum.reverse()
+    right = array |> Enum.at(x) |> Enum.slice(y + 1, l)
+    left = array |> Enum.at(x) |> Enum.slice(0, y) |> Enum.reverse()
 
     [up, down, right, left]
   end
 
-  defp neighbours_one(x, y, l, array) do
-    up =
-      for i <- Range.new(0, x - 1) do
-        Enum.at(array, i) |> Enum.at(y)
-      end
+  defp calc_dist(parts, e) do
+    parts
+    |> Enum.map(fn el -> dist(el, e) end)
+    |> Enum.product()
+  end
 
-    down =
-      for i <- Range.new(x + 1, l) do
-        Enum.at(array, i) |> Enum.at(y)
-      end
-
-    right = Enum.at(array, x) |> Enum.slice(y + 1, l - 1)
-    left = Enum.at(array, x) |> Enum.slice(0, y)
-
-    [up, down, right, left]
+  defp dist(list, e) do
+    Enum.reduce_while(list, 0, fn x, acc ->
+      if x >= e, do: {:halt, acc + 1}, else: {:cont, acc + 1}
+    end)
   end
 
   defp parse(input) do
-    input
-    |> String.split("\r\n", trim: true)
-    |> Enum.map(fn line -> String.codepoints(line) end)
-    |> Enum.map(fn list ->
-      for s <- list do
-        String.to_integer(s)
-      end
-    end)
+    array =
+      input
+      |> String.split("\r\n", trim: true)
+      |> Enum.map(fn line -> String.codepoints(line) end)
+      |> Enum.map(fn l -> Enum.map(l, fn x -> String.to_integer(x) end) end)
+
+    {array, Enum.count(array) - 1}
   end
 end
